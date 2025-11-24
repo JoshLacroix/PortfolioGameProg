9deaa3f4929f665ffc60580b3afe93e031432bfd
Josh Lacroix <joshlacroix@outlook.com>
2025-11-24 17:07:08 -0500

    Forest Scene added, UI Updated

--- Scenes/portfolio.gd
+++ Scenes/portfolio.gd
@@ -2,15 +2,19 @@ extends Control
 
 @onready var prison_escape_scene: PackedScene = preload("res://Prison/Scenes/Prison/prison_scene.tscn")
 @onready var ninja_parkour_Scene: PackedScene = preload("res://Ninja/Scenes/stage.tscn")
+@onready var forest_scene: PackedScene = preload("res://Forest/Scenes/world.tscn")
 
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
-
+func _ready() -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
 
 func _on_prison_button_pressed() -> void:
 	get_tree().change_scene_to_packed(prison_escape_scene)
 
-
 func _on_ninja_button_pressed() -> void:
 	get_tree().change_scene_to_packed(ninja_parkour_Scene)
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+func _on_forrest_button_pressed() -> void:
+	get_tree().change_scene_to_packed(forest_scene)
